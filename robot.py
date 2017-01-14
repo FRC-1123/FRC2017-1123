dd64047850e6dc6e88d7fb4a815bd001a020c117
czhao <czhao39@gmail.com>
2017-01-14 17:36:12 -0500

    proof-of-concept robot.py with arcade drive

---
+++ robot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import wpilib
+import ctre
+
+
+class Robot(wpilib.IterativeRobot):
+
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.robot_drive = wpilib.RobotDrive(ctre.CANTalon(1), ctre.CANTalon(2))
+        self.stick = wpilib.Joystick(1)
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        self.auto_loop_counter = 0
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+        # Check if we've completed 100 loops (approximately 2 seconds)
+        if self.auto_loop_counter < 100:
+            self.robot_drive.drive(-0.5, 0)  # Drive forwards at half speed
+            self.auto_loop_counter += 1
+        else:
+            self.robot_drive.drive(0, 0)  # Stop robot
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        self.robot_drive.arcadeDrive(self.stick)
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        wpilib.LiveWindow.run()
+
+
+if __name__ == "__main__":
+    wpilib.run(Robot)
