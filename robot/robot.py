8edda62b1e6e25113bfee26dcc16a8e9da370f06
czhao <czhao39@gmail.com>
2017-01-16 13:22:06 -0500

    fix throttling and add buttons for turning in place

--- robot/robot.py
+++ robot/robot.py
@@ -13,10 +13,13 @@ class Robot(wpilib.IterativeRobot):
         left_motor = ctre.CANTalon(0)
         right_motor = ctre.CANTalon(1)
         self.robot_drive = wpilib.RobotDrive(left_motor, right_motor)
-        self.robot_drive.setMaxOutput(.3)
+        self.robot_drive.setMaxOutput(.5)
 
         self.stick = wpilib.Joystick(0)
         self.controller = wpilib.XboxController(0)
+
+        self.printTimer = wpilib.Timer()
+        self.printTimer.start()
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
@@ -35,7 +38,11 @@ class Robot(wpilib.IterativeRobot):
         """This function is called periodically during operator control."""
         self.robot_drive.tankDrive(self.stick, 1, self.stick, 5)  # 1 and 5 are left and right joystick axes
         if self.controller.getAButton():
-            self.robot_drive.drive(-.1, 0)
+            self.robot_drive.drive(-.5, 0)  # move forward slowly
+        if self.controller.getXButton():
+            self.robot_drive.setLeftRightMotorOutputs(-.5, -.5)
+        elif self.controller.getYButton():
+            self.robot_drive.setLeftRightMotorOutputs(.5, .5)
 
     def testPeriodic(self):
         """This function is called periodically during test mode."""
