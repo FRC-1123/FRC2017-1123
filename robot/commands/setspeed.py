b009ef8637198510b6d531557f62072c088dd959
Haicheng Charles Zhao <czhao39@gmail.com>
2017-02-04 18:53:42 -0500

    use RectifiedDrive for SetSpeed command

--- robot/commands/setspeed.py
+++ robot/commands/setspeed.py
@@ -2,6 +2,7 @@ import wpilib
 from wpilib.command import TimedCommand
 
 import subsystems
+from rectifieddrive import RectifiedDrive
 
 
 class SetSpeed(TimedCommand):
@@ -14,12 +15,15 @@ class SetSpeed(TimedCommand):
 
         self.power = power
         self.requires(subsystems.motors)
+        
+        self.drive = RectifiedDrive(0)
 
         self.timer = wpilib.Timer()
         self.timer.start()
 
     def execute(self):
-        subsystems.motors.setSpeed(self.power)
+        self.drive.rectified_drive(self.power, 0)
+        # subsystems.motors.setSpeed(self.power)
         self.timer.delay(0.05)
 
     def end(self):
