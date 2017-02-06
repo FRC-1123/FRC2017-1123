c6087e096bc136ae2c041f7c0a36190b193c0ff7
Grey Golla <bobobalink@gmail.com>
2017-02-06 13:15:46 -0500

    played with more useful tests

--- robot/tests/driving_test.py
+++ robot/tests/driving_test.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from pyfrc.test_support import controller
 import robotmap
 
@@ -23,9 +25,24 @@ def test_driving(hal_data: controller.hal_data, control: controller.TestControll
 
 
 # def test_testing(hal_data: controller.hal_data, control: controller.TestController, fake_time, robot):
-#     print(len(hal_data['joysticks']), str(hal_data['joysticks']))
-#     for joy in hal_data['joysticks']:
-#         print(str(joy))
-#         for i, axis in joy.items():
-#             print('    ' + i + ': ' + str(axis))
+#
+#     def tController(time):
+#         hal_data['joysticks'][robotmap.joystick.port]['axes'][robotmap.joystick.forwardAxis] = 0.1
+#         hal_data['joysticks'][robotmap.joystick.port]['axes'][robotmap.joystick.steeringAxis] = 0.5
+#         for x in hal_data['CAN']:
+#             print(x, hal_data['CAN'][x]['value'])
+#         if time > 2:
+#             return False
+#         return True
+#
+#     control.set_operator_control(enabled=True)
+#     control.run_test(tController)
+#
+#     for x in hal_data:
+#         print(x)
+#
+#     print('==== CAN ====')
+#     for x in hal_data['CAN']:
+#         print(x, hal_data['CAN'][x]['value'])
+#         # pprint(hal_data['CAN'][x])
 #     assert False
