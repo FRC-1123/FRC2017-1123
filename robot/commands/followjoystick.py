77048803d31e3d1c7ab85dfaddd3baec4978862b
czhao <czhao39@gmail.com>
2017-02-06 19:35:25 -0500

    fix rectified drive steering when moving backwards

--- robot/commands/followjoystick.py
+++ robot/commands/followjoystick.py
@@ -38,5 +38,7 @@ class FollowJoystick(Command):
         # rectified arcade drive
         power = oi.joystick.getRawAxis(robotmap.joystick.forwardAxis)
         angular_vel = -oi.joystick.getRawAxis(robotmap.joystick.steeringAxis)
+        if power > 0:  # if moving backwards, negate angular velocity
+            angular_vel *= -1
         self.logger.info("{}\t{}".format(angular_vel, power))
         self.drive.rectified_drive(power, angular_vel)
