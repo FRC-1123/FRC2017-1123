be38f35af82d9235ac5b24c1029442fb42367e0c
czhao <czhao39@gmail.com>
2017-02-13 18:06:36 -0500

    make sure to kill pynetworktables2js on exit

--- startdashboard.sh
+++ startdashboard.sh
@@ -1,3 +1,11 @@
 cd dashboard/
 pynetworktables2js --robot=roborio-1123-frc.local &
+PID=$!
+
+# kill pynetworktables2js on EXIT
+trap cleanup EXIT
+function cleanup() {
+    kill $PID &> /dev/null
+}
+
 npm start
