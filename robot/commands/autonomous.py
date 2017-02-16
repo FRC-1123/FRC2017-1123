a922fe83e752eaab195f4b54e1e587d1245a1d34
Haicheng Charles Zhao <czhao39@gmail.com>
2017-02-15 23:01:11 -0500

    make autonomous drive forward a bit before rotating

--- robot/commands/autonomous.py
+++ robot/commands/autonomous.py
@@ -2,6 +2,7 @@ from wpilib.command.commandgroup import CommandGroup
 
 from commands.drivetorod import DriveToRod
 from commands.rotate import Rotate
+from commands.setspeed import SetSpeed
 
 
 class AutonomousProgram(CommandGroup):
@@ -9,10 +10,12 @@ class AutonomousProgram(CommandGroup):
         super().__init__('Autonomous Program')
 
         if mode == "left":
+            self.addSequential(SetSpeed(0.3, 1))
             self.addSequential(Rotate(30))
             self.addSequential(DriveToRod())
         elif mode == "right":
-            self.addSequential(Rotate(30))
+            self.addSequential(SetSpeed(0.3, 1))
+            self.addSequential(Rotate(-30))
             self.addSequential(DriveToRod())
         else:
             self.addSequential(DriveToRod())
