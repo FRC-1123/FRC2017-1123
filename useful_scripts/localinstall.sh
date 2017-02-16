b7113cf725e3bc40ddb5e0958b024f7a6eb403dd
Haicheng Charles Zhao <czhao39@gmail.com>
2017-02-15 22:00:31 -0500

    create basic local install script

---
+++ useful_scripts/localinstall.sh
@@ -0,0 +1,7 @@
+# Installs the dashboard and everything needed to deploy robot code.
+# Make sure you have installed Python 3 and Node.js before running this.
+# If you wish to use OpenCV, you will have to install it yourself (make sure to get OpenCV 3).
+
+pip install -r ../requirements.txt
+cd ../dashboard/
+npm install
