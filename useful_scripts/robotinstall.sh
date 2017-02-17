9e6a1421615b90fb603147df165fa8e512329698
czhao <czhao39@gmail.com>
2017-02-17 17:15:44 -0500

    clean up robot installer

--- useful_scripts/robotinstall.sh
+++ useful_scripts/robotinstall.sh
@@ -5,7 +5,7 @@
 # You may have to alter version numbers (see https://www.tortall.net/~robotpy/feeds/).
 
 robotpy-installer download-robotpy
-robotpy-installer download-opkg python36-robotpy-ctre python36-numpy opencv3 python36-opencv3 python36-robotpy-cscore mjpg-streamer
+robotpy-installer download-opkg python36-robotpy-ctre python36-robotpy-cscore mjpg-streamer
 
 robotpy-installer install-robotpy
-robotpy-installer install-opkg python36-robotpy-ctre python36-numpy opencv3 python36-opencv3 python36-robotpy-cscore mjpg-streamer
+robotpy-installer install-opkg python36-robotpy-ctre python36-robotpy-cscore mjpg-streamer
