0f8b7fd1a3bb04691f02f14e1443c3c817708b6f
czhao <czhao39@gmail.com>
2017-02-18 20:49:35 -0500

    add ping timer to not unnecessarily ping range finders

--- robot/inputs/sonar.py
+++ robot/inputs/sonar.py
@@ -19,14 +19,16 @@ pinger = None
 
 distances = [0, 0, 0, 0, 0, 0, 0, 0]
 speeds = [0, 0, 0, 0, 0, 0, 0, 0]  # speeds of approach
-timer = Timer()
+speed_timer = Timer()
+ping_timer = Timer()
+
 prev_time = 0
 
 def init():
     """
     Initialize sonar object.
     """
-    global front, front_right, right, back_right, back, back_left, left, front_left, pinger, timer
+    global front, front_right, right, back_right, back, back_left, left, front_left, pinger, speed_timer
 
     front = MaxSonarEZPulseWidth(robotmap.sonar.front_channel, inch)
     front_right = MaxSonarEZPulseWidth(robotmap.sonar.front_right_channel, inch)
@@ -38,18 +40,19 @@ def init():
     front_left = MaxSonarEZPulseWidth(robotmap.sonar.front_left_channel, inch)
 
     pinger = DigitalOutput(robotmap.sonar.pinger_channel)
-    timer.start()
+    speed_timer.start()
+    ping_timer.start()
 
 
 def update_readings():
-    if pinger is not None:
+    if pinger is not None and ping_timer.hasPeriodPassed(0.05):  # radars can update up to every 50 ms
         pinger.pulse(0.0001)  # pulse for 100 microseconds
         new_dists = [front.get(), front_right.get(), right.get(), back_right.get(), back.get(), back_left.get(),
                      left.get(), front_left.get()]
         global prev_time
-        time_elapsed = timer.get() - prev_time
-        if time_elapsed > 0.1:  # update speeds every 0.2 seconds
+        time_elapsed = speed_timer.get() - prev_time
+        if time_elapsed > 0.1:  # update speeds every 0.1 seconds
             global speeds, distances
             speeds = [(distances[i] - new_dists[i]) / time_elapsed for i in range(len(speeds))]
-            prev_time = timer.get()
+            prev_time = speed_timer.get()
         distances = new_dists
