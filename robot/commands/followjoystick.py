e99ed1f234adca7665e2e9464921d3db57a7727f
czhao <czhao39@gmail.com>
2017-03-04 22:58:22 -0500

    depress angular velocity when bumper pressed

--- robot/commands/followjoystick.py
+++ robot/commands/followjoystick.py
@@ -44,6 +44,7 @@ class FollowJoystick(Command):
             power /= oi.divider  # for limiting power
 
             angular_vel = oi.joystick.getRawAxis(robotmap.joystick.steeringAxis)
+            angular_vel /= oi.divider  # for limiting angular velocity
             # if power > 0:  # if moving backwards, negate angular velocity
             #     angular_vel *= -1
             self.drive.rectified_drive(power, angular_vel)
