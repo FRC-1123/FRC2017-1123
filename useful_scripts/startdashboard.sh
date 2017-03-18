b8de7edc1b5482efe67ab7b3c8d1a96468db4e50
czhao <czhao39@gmail.com>
2017-03-18 12:22:21 -0400

    update dashboard startup script with IP's for competition and development

--- useful_scripts/startdashboard.sh
+++ useful_scripts/startdashboard.sh
@@ -2,7 +2,12 @@
 # Starts the dashboard and cleans up on exit.
 
 cd "$(dirname "$(readlink -f $0)")/../dashboard/"
-pynetworktables2js --robot=10.11.23.2 &
+
+# DURING COMPETITION, UNCOMMENT THE LINE BELOW AND COMMENT THE LINE BELOW IT.
+# ALSO, MAKE SURE THE DRIVER STATION'S STATIC IP IS SET TO 10.11.23.6
+# pynetworktables2js --robot=10.11.23.2 &
+pynetworktables2js --robot=roborio-1123-frc.local &
+
 PID=$!
 
 # kill pynetworktables2js on EXIT
